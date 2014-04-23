ce6bc4baaaf610c82843c61d08050586984097af
MrRatermat <MrRatermat@users.noreply.github.com>
2014-04-23 15:13:54 +0100

    Update ai.lua

--- modules/ai.lua
+++ modules/ai.lua
@@ -4,18 +4,18 @@ local escaped_nickname = escape_lua_pattern(nickname)
 enabled = false
 function ai(line)
     if enabled == true then --Enabled
-        local typemsg,nick,channel,txt=getMsgType(line)
         if not txt then return nil end --Has content
         if not txt:match("^"..commandPrefix.."(.*)") then --Not a command
+            local typemsg,nick,channel,txt=getMsgType(line)
             local reply = ""
-            reply = Think(txt, nick)
+            reply = think(txt, nick)
             if not reply == "" then
                 send("PRIVMSG "..channel.." :"..reply)
             end
         end
     end
 end
-function aiInit(text,nick,channel)
+function init(text,nick,channel)
     if enabled == true then
         enabled = false
         return "AI stopped"
@@ -24,13 +24,13 @@ function aiInit(text,nick,channel)
         return "AI started"
     end
 end
-commands["ai"] = aiInit
-loopCalls["aiWatcher"] = ai
+commands["ai"] = init
+loopCalls["ai"] = ai
 
 --AI code
 local replies = {"Hello","Hi","Ohia","Hey"}
 local postfixes = {"!","..."," :D"}
-local function Think(text, user)
+local function think(text, user)
     if not string.find(text,escaped_nickname) == nil then
         seed()
         if not string.find(text,escaped_nickname) == nil then
