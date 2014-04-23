84f39eb57788a14c7e6ec21577093d5db0a1f750
vifino <adinator1999@gmail.com>
2014-04-23 14:35:43 +0200

    Update answers.lua

--- libs/answers.lua
+++ libs/answers.lua
@@ -1,22 +1,22 @@
 -- Answers to common situations
 -- Made by vifino
 function no()
-	random.seed()
+	seed()
 	local nou = {"No.", "Nou", "Nope!","nope.avi","http://not.inUrDreamz.com","Noo!","Nu.","Nu","No way.","Try better next time.", "lelno","Nupe!"}
 	return nou[math.random(1,#nou)]
 end
 function yes()
-	random.seed()
+	seed()
 	local yes = {"Yes","Definitly","Sure","Yes!","Yup.","Yeah!"}
 	return yes[math.random(1,#yes)]
 end
 function maybe()
-	random.seed()
+	seed()
 	local maybe = {"Maybe...","Depends...","May, under certain Situations."}
 	return maybe[math.random(1,#maybe)]
 end
 function theAnswer()
-	random.seed()
+	seed()
 	local answers = {"42.", "Forty Two.","Forty 2.","40 + 2"}
 	return answers[math.random(1,#answers)]
 end
