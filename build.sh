92a3a784c3b010610d97eccfc5d646d9febf46ba
Adrian Pistol <adinator1999@gmail.com>
2014-06-21 20:18:33 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,9 +5,12 @@ cd $DIR
 echo "Updating to latest versiom..."
 git pull && git submodule init && git submodule update
 chmod +x hecta
+chmod +x *.sh
+cp settingsExample.lua settings.lua
 #cd twinpipe && make
 #echo "You might need to install twinpipe. To do that, just cd to ./twinpipe, and run make install as root."
 #cd ..
 #cp twinpipe/twinpipe twinpipe/usleep .
 
 echo "Done! You should be good to go now, if you have Lua, screen, and either LuaSocket or netcat."
+echo "Remember: Tweak the settings in settings.lua to fit your needs!"
