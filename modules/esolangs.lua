ec96d725721c969b26cdaa4e8797435a97b8f7b5
vifino <adinator1999@gmail.com>
2014-06-22 00:07:54 +0200

    Esolangs: Improved DeadFish+

--- modules/esolangs.lua
+++ modules/esolangs.lua
@@ -118,27 +118,31 @@ function deadfish(input)
 	end
 end
 commands["deadfish"] = deadfish
+commands["df"] = deadfish
 function deadfishplus(input) -- is only improved, or extended deadfish
 	if type(input) == "string" then
-		local output = ""
-		local number = 0
-		string.gsub(input,".",function(char)
-			if number > 256 or number < 0 then number = 0 end
-			if char == "d" then
-				number = number - 1
-			elseif char == "i" then
-				number = number + 1
-			elseif char == "o" then
-				output = output..tostring(number)
-			elseif char == "s" then
-				number = number * number
-			elseif char == "c" then
+		local function eval(input) 
+			local output = ""
+			local number = 0
+			string.gsub(input:lower(),".",function(char)
+				if number > 255 or number < 0 then number = 0 end
+				if char == "d" then
+					number = number - 1
+				elseif char == "i" then
+					number = number + 1
+				elseif char == "o" then
+					output = output..tostring(number)
+				elseif char == "s" then
+					number = number * number
+				elseif char == "c" then
 				output = output..string.char(number)
-			elseif char == "r" then
-				number = 0
-			end
-		end)
-		return output
+				elseif char == "r" then
+					number = 0
+				end
+			end)
+		end
+		return eval(input)
 	end
 end
 commands["deadfish+"] = deadfishplus
+commands["df+"] = deadfishplus
