96b2ebda4757224590e672d6ba4a59c8caac7423
vifino <adinator1999@gmail.com>
2014-07-18 21:57:42 +0200

    Added gizoogle module

---
+++ modules/gizoogle.lua
@@ -0,0 +1,8 @@
+-- Gizoogle! Fo Shizzles!
+-- Made by vifino
+gizoogle = {}
+function gizoogle.translate(text)
+	local s=http.request("http://www.gizoogle.net/textilizer.php","translatetext="..text)
+	return s:match("<textarea type=\"text\" name=\"translatetext\" style=\"width: 600px; height:250px;\"/>(.-)</textarea>"
+end
+commands["gizoogle"] = gizoogle
