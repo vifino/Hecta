c36a24215f7e2e4fb1412edfc028484ef53cca5f
vifino <adinator1999@gmail.com>
2014-07-18 22:00:50 +0200

    I tont wanna talk about that.

--- modules/gizoogle.lua
+++ modules/gizoogle.lua
@@ -3,6 +3,6 @@
 gizoogle = {}
 function gizoogle.translate(text)
 	local s=http.request("http://www.gizoogle.net/textilizer.php","translatetext="..text)
-	return s:match("<textarea type=\"text\" name=\"translatetext\" style=\"width: 600px; height:250px;\"/>(.-)</textarea>"
+	return s:match("<textarea type=\"text\" name=\"translatetext\" style=\"width: 600px; height:250px;\"/>(.-)</textarea>")
 end
 commands["gizoogle"] = gizoogle.translate
