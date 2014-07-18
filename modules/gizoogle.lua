fa057c4820d6f1437b8d65266508cc63e6955218
vifino <adinator1999@gmail.com>
2014-07-18 21:59:01 +0200

    Fixed gizoogle translate

--- modules/gizoogle.lua
+++ modules/gizoogle.lua
@@ -5,4 +5,4 @@ function gizoogle.translate(text)
 	local s=http.request("http://www.gizoogle.net/textilizer.php","translatetext="..text)
 	return s:match("<textarea type=\"text\" name=\"translatetext\" style=\"width: 600px; height:250px;\"/>(.-)</textarea>"
 end
-commands["gizoogle"] = gizoogle
+commands["gizoogle"] = gizoogle.translate
