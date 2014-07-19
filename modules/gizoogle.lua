5eeb2cd9e388bc975229afe751f3d7c4478573c4
vifino <adinator1999@gmail.com>
2014-07-19 14:27:52 +0200

    ver 2 of gizoogle textilize

--- modules/gizoogle.lua
+++ modules/gizoogle.lua
@@ -1,8 +1,31 @@
 -- Gizoogle! Fo Shizzles!
 -- Made by vifino
+http = require"socket.http"
+ltn12 = require"ltn12"
 gizoogle = {}
-function gizoogle.translate(text)
+function gizoogle.textilize(text)
 	local s=http.request("http://www.gizoogle.net/textilizer.php","translatetext="..text)
 	return s:match("<textarea type=\"text\" name=\"translatetext\" style=\"width: 600px; height:250px;\"/>(.-)</textarea>")
 end
-commands["gizoogle"] = gizoogle.translate
+function gizoogle.textilize2(text)
+	if text then
+		local reqbody = "translatetext="..tostring(text)
+		local respbody = {} -- for the response body
+		local result, respcode, respheaders, respstatus = http.request {
+			method = "POST",
+			url = "http://www.gizoogle.net/textilizer.php",
+			source = ltn12.source.string(reqbody),
+			headers = {
+				["content-type"] = "text/plain",
+				["content-length"] = tostring(#reqbody)
+			},
+			sink = ltn12.sink.table(respbody)
+		}
+		-- get body as string by concatenating table filled by sink
+		respbody = table.concat(respbody)
+		return respbody
+	else
+		return nil
+	end
+end
+commands["gizoogle"] = gizoogle.textilize
